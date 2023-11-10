a15e55ab56fbe6ead4cb14cb71679c5df668afb1
Bodo Schulz <bodo@boone-schulz.de>
2023-11-10 06:27:01 +0100

    update build hooks

--- hooks/tox.sh
+++ hooks/tox.sh
@@ -12,12 +12,38 @@ then
 
     if [ -z "${collections_installed}" ]
     then
-      echo "Install the required collection '${collection}'"
-      ansible-galaxy collection install ${collection}
+      install_collection ${collection}
     else
       collection_version=$(echo "${collections_installed}" | awk -F ' ' '{print $2}')
+      version="$(grep -v "#" collections.yml | grep -A1 "^  - name: ${collection}" | grep "version: " 2> /dev/null | awk -F ': ' '{print $2}' | sed -e 's|=||' -e 's|>||' -e 's|"||g')"
 
       echo "The required collection '${collection}' is installed in version ${collection_version}."
+
+      if [ ! -z "${version}" ]
+      then
+
+        vercomp "${version}" "${collection_version}"
+
+        case $? in
+          0) op='=' ;;
+          1) op='>' ;;
+          2) op='<' ;;
+        esac
+
+        if [[ $op = "=" ]] || [[ $op = ">" ]]
+        then
+          # echo "FAIL: Expected '$3', Actual '$op', Arg1 '$1', Arg2 '$2'"
+          echo "re-install for version ${version}"
+
+          remove_collection ${collection}
+          install_collection ${collection}
+        else
+          :
+          # echo "Pass: '$1 $op $2'"
+        fi
+      else
+        :
+      fi
     fi
   done
   echo ""
